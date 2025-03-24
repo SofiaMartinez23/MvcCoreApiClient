71ebfdcb10664d234a5fc0573916a76e00811266
SofiaMartinez23 <sofiamg2392001@gmail.com>
2025-03-24 09:10:24 +0100

    Add project files.

---
+++ MvcCoreApiClient/Services/ServiceHospitales.cs
@@ -0,0 +1,42 @@
+﻿using MvcCoreApiClient.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace MvcCoreApiClient.Services
+{
+    public class ServiceHospitales
+    {
+        private string ApiUrl;
+        private MediaTypeWithQualityHeaderValue header;
+
+        public ServiceHospitales()
+        {
+            this.ApiUrl = "https://apicorehospitalessmg.azurewebsites.net/";
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        public async Task<List<Hospital>> GetHospitalesAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/hospitales";
+                client.BaseAddress = new Uri(this.ApiUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+
+                HttpResponseMessage response = await client.GetAsync(request);
+
+                if(response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    List<Hospital> data = JsonConvert.DeserializeObject<List<Hospital>>(json);
+                    return data;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        } 
+    }
+}
